e67c40323666376f1bfb6bf812fd1fbd78ddea7e
Craig Stevens <craig_stevens@dell.com>
2018-10-27 17:03:33 -0600

    Added onlp_psui_hdr_get function and fixed up status and presence

--- packages/platforms/dell/x86-64/z9100/onlp/builds/x86_64_dell_z9100_c2538/module/src/psui.c
+++ packages/platforms/dell/x86-64/z9100/onlp/builds/x86_64_dell_z9100_c2538/module/src/psui.c
@@ -26,19 +26,19 @@
 #include <onlp/platformi/base.h>
 #include "platform_lib.h"
 
-#define PSU_PRESENCE_MASK     0x01
-#define PSU_POWER_TYPE_MASK   0x02
-#define PSU_STATUS_MASK       0x08
+typedef struct psu_cfg {
+  uint16_t addr;
+  uint8_t presence_mask;
+  uint16_t power_type_mask;
+  uint8_t status_mask;
+  uint8_t data;
+} psu_cfg_t;
 
-#define PSU_FAN_AIRFLOW_MASK  0x01
-#define PSU_FAN_STATUS_MASK   0x02
-#define PSU_FAN_PRESENCE_MASK 0x04
-
-/* SF IO Bus addresses */
-#define PSU0_STATUS_ADDR     0x237 /* psu0 presence, pwr-type, status */
-#define PSU0_FAN_STATUS_ADDR 0x23d /* psu0 fan air flow, status, presence */
-#define PSU1_STATUS_ADDR     0x270 /* psu1 presence, pwr-type, status */
-#define PSU1_FAN_STATUS_ADDR 0x276 /* psu1 fan air flow, status, presence */
+psu_cfg_t psus_cfg[] = {
+    { },                                /* Not Uses */
+    { 0x237, 0x01, 0x02, 0x08, 0x00 },  /* PSU 0 */
+    { 0x270, 0x01, 0x02, 0x08, 0x00 }   /* PSU 1 */
+};
 
 /*
  * Get all information about the given PSU oid.
@@ -55,41 +55,46 @@ static onlp_psu_info_t pinfo[] =
 };
 
 int
-onlp_psui_info_get(onlp_oid_t id, onlp_psu_info_t* info)
+onlp_psui_hdr_get(onlp_oid_t oid, onlp_oid_hdr_t* hdr)
 {
-    int ret    = ONLP_STATUS_OK;
-    int index  = ONLP_OID_ID_GET(id);
-    uint16_t addr   = 0;
-    uint8_t data = 0;
+    int id  = ONLP_OID_ID_GET(oid);
 
-    *info = pinfo[index]; /* Set the onlp_oid_hdr_t */
-
-    /* Get the present state */
-    if (PSU1_ID == id) {
-      addr = PSU0_STATUS_ADDR;
-    } else {
-      addr = PSU1_STATUS_ADDR;
-    }
+    *hdr = pinfo[id].hdr; /* Set the onlp_oid_hdr_t */
 
     /* read the register from the CPLD */
     ONLP_IF_ERROR_RETURN(smbus_set_perms());
-    ONLP_IF_ERROR_RETURN(smbus_read_byte(addr, &data));
+    ONLP_IF_ERROR_RETURN(smbus_read_byte(psus_cfg[id].addr, &psus_cfg[id].data));
     ONLP_IF_ERROR_RETURN(smbus_unset_perms());
 
     /* See if present and status ok */
-    if ((data&PSU_PRESENCE_MASK) != 0) {
-      ONLP_OID_STATUS_FLAG_CLR(info, PRESENT);
+    if ((psus_cfg[id].data&psus_cfg[id].presence_mask) != 0) {
+      ONLP_OID_STATUS_FLAG_CLR(hdr, PRESENT);
       return ONLP_STATUS_OK;
     }
-    ONLP_OID_STATUS_FLAG_SET(info, PRESENT);
+    ONLP_OID_STATUS_FLAG_SET(hdr, PRESENT);
 
     /* Status ok */
-    if ((data&PSU_STATUS_MASK) == 0) {
-      ONLP_OID_STATUS_FLAG_SET(info, FAILED);
+    if ((psus_cfg[id].data&psus_cfg[id].status_mask) == 0) {
+      ONLP_OID_STATUS_FLAG_SET(hdr, FAILED);
+    } else {
+      ONLP_OID_STATUS_FLAG_CLR(hdr, FAILED);
     }
 
+    return ONLP_STATUS_OK;
+}
+
+int
+onlp_psui_info_get(onlp_oid_t oid, onlp_psu_info_t* info)
+{
+    int id  = ONLP_OID_ID_GET(oid);
+
+    *info = pinfo[id]; /* Set the onlp_oid_hdr_t */
+
+    /* Get hdr */
+    ONLP_IF_ERROR_RETURN(onlp_psui_hdr_get(oid, &info->hdr));
+
     /* Get PSU type     */
-    if ((data&PSU_POWER_TYPE_MASK) == 0) {
+    if ((psus_cfg[id].data&psus_cfg[id].power_type_mask) == 0) {
       info->caps = ONLP_PSU_CAPS_GET_TYPE;
       info->type = ONLP_PSU_TYPE_AC;
     } else {
@@ -99,5 +104,5 @@ onlp_psui_info_get(onlp_oid_t id, onlp_psu_info_t* info)
 
     /* Can't get voltage, amps, power on Z9100 */
 
-    return ret;
+    return ONLP_STATUS_OK;
 }
